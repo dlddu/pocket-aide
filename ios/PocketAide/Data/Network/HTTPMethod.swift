8b079936182d869b4ff739d8bf90de94a4391128
silas <39251873+dlddu@users.noreply.github.com>
2026-03-15 07:59:42 +0900

    Refactor credential handling and extend MemoServiceProtocol (#23)

--- ios/PocketAide/Data/Network/HTTPMethod.swift
+++ ios/PocketAide/Data/Network/HTTPMethod.swift
@@ -3,6 +3,8 @@
 
 /// HTTP 요청 메서드를 나타내는 열거형.
 public enum HTTPMethod: String {
-    case get  = "GET"
-    case post = "POST"
+    case get    = "GET"
+    case post   = "POST"
+    case put    = "PUT"
+    case delete = "DELETE"
 }
